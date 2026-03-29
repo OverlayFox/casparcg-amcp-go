06ed730115d9917f1af9b9508c82e9cb495138bf
OverlayFox <info@overlayfox.com>
2026-03-29 16:52:33 +0200

    expanded get Info examples

--- cmd/getInfo/main.go
+++ cmd/getInfo/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 
 	"github.com/overlayfox/casparcg-amcp-go"
-	"github.com/overlayfox/casparcg-amcp-go/types"
 )
 
 func main() {
@@ -21,7 +21,15 @@ func main() {
 		}
 	}()
 
-	err = client.CG().Channel(1).Layer(1).CGLayer(1).Add(types.CGAdd{Template: "L3", PlayOnLoad: true, Data: jsonString})
+	// Ping server to see if it's alive and responding
+	pingResp, err := client.Ping(ptr("CasparCG AMCP Go Client - Info Example\n\n\n"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Ping response:", pingResp)
+
+	// Get generic info about the server and print it as JSON
+	resp, err := client.Query().Info().Generic()
 	if err != nil {
 		var casparErr casparcg.CasparCGError
 		if errors.As(err, &casparErr) {
@@ -29,10 +37,81 @@ func main() {
 		}
 		panic(err)
 	}
+	jsonData, err := json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Generic Info:")
+	fmt.Println(string(jsonData) + "\n\n\n")
 
-	// jsonData, err := json.MarshalIndent(data, "", "  ")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(string(jsonData))
+	// Some functions are marked as "deprecated" but they still work.
+	// They just won't return sensible data.
+	resp, err = client.Query().Info().Queues()
+	if err != nil {
+		var casparErr casparcg.CasparCGError
+		if errors.As(err, &casparErr) {
+			fmt.Printf("CasparCG error: %d - %s\n", casparErr.Code, casparErr.Message)
+		}
+		panic(err)
+	}
+	jsonData, err = json.MarshalIndent(resp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Deprecated Queues Info:")
+	fmt.Println(string(jsonData) + "\n\n\n")
+
+	// Get config info about the server and print it as JSON
+	configResp, err := client.Query().Info().Config()
+	if err != nil {
+		var casparErr casparcg.CasparCGError
+		if errors.As(err, &casparErr) {
+			fmt.Printf("CasparCG error: %d - %s\n", casparErr.Code, casparErr.Message)
+		}
+		panic(err)
+	}
+	jsonData, err = json.MarshalIndent(configResp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Config Info:")
+	fmt.Println(string(jsonData) + "\n\n\n")
+
+	// Some query commands were moved to different builders to better reflect their purpose.
+	layerResp, err := client.Layer().Channel(1).Info().Generic()
+	if err != nil {
+		var casparErr casparcg.CasparCGError
+		if errors.As(err, &casparErr) {
+			fmt.Printf("CasparCG error: %d - %s\n", casparErr.Code, casparErr.Message)
+		}
+		panic(err)
+	}
+	jsonData, err = json.MarshalIndent(layerResp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Layer Channel Info:")
+	fmt.Println(string(jsonData) + "\n\n\n")
+
+	// Some query commands were moved to different builders to better reflect their purpose.
+	layerResp, err = client.Layer().Channel(1).Layer(10).Info().Generic()
+	if err != nil {
+		var casparErr casparcg.CasparCGError
+		if errors.As(err, &casparErr) {
+			fmt.Printf("CasparCG error: %d - %s\n", casparErr.Code, casparErr.Message)
+		}
+		panic(err)
+	}
+	jsonData, err = json.MarshalIndent(layerResp, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Layer Channel Layer Info:")
+	fmt.Println(string(jsonData) + "\n\n\n")
+
+	fmt.Println("Done.")
+}
+
+func ptr[T any](v T) *T {
+	return &v
 }
